77d6569ddc62b1fdba862e2a33b1d51191b7a134
ssnepenthe <ssnepenthe@users.noreply.github.com>
2015-12-16 10:07:40 -0700

    Use the base selector for instructions

--- src/Parsers/BHGCom.php
+++ src/Parsers/BHGCom.php
@@ -13,31 +13,4 @@ class BHGCom extends SchemaOrg {
 		$this->paths['image'] = './/*[@itemprop="thumbnail"]';
 		$this->paths['name'] = './/h1[@itemprop="name"]';
 	}
-	protected function parse_recipe_instructions() {
-		$nodes = $this->xpath->query( $this->paths['recipeInstructions'], $this->schema_root->item( 0 ) );
-
-		if ( ! $nodes->length ) {
-			$nodes = $this->xpath->query( $this->paths['recipeInstructions'] );
-		}
-
-		if ( $nodes->length ) {
-			foreach ( $nodes as $node ) {
-				if ( $node->nodeValue ) {
-					$value = trim( $node->nodeValue );
-					$value = $this->normalize_newlines( $value );
-					$value = $this->normalize_whitespace( $value );
-
-					$value = array_map( 'trim', explode( "\n", $value ) );
-
-					foreach ( $value as $instruction ) {
-						$this->recipe->add_recipe_instruction( $instruction );
-					}
-				}
-			}
-
-			unset( $node, $value, $instruction );
-		}
-
-		unset( $nodes );
-	}
 }
