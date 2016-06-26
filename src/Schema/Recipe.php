eb9e3d8c166d66bd099596e342e9b1f286adb63e
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-26 12:52:40 -0700

    First pass at recipe schema hierarchy.

--- src/Schema/Recipe.php
+++ src/Schema/Recipe.php
@@ -1,43 +1,205 @@
 <?php
+/**
+ * Schema.org Recipe. Not complete and modified slightly to better match
+ * implementations found in the wild.
+ */
 
 namespace SSNepenthe\RecipeParser\Schema;
 
-class Recipe {
-	protected $attributes = [];
+/**
+ * This class represents a Recipe object as defined by schema.org. It has been
+ * paired down to only include properties useful when scraping recipes from
+ * around the web.
+ *
+ * @link https://schema.org/Recipe
+ *
+ * @todo Consider how we could support recipe notes, maybe as 'comment' property.
+ *       Consider adding support for the 'nutrition' property.
+ *       Consider adding support for the 'suitableForDiet' property.
+ */
+class Recipe extends CreativeWork
+{
+    public function __construct()
+    {
+        parent::__construct();
 
-	public function __construct() {
-		$keys = [
-			'author',
-			'cook_time',
-			'description',
-			'image',
-			'name',
-			'prep_time',
-			'publisher',
-			'recipe_categories',
-			'recipe_ingredients',
-			'recipe_instructions',
-			'recipe_yield',
-			'total_time',
-			'url',
-		];
+        $keys = [
+            'cookTime',
+            'cookingMethod',
+            'prepTime',
+            'recipeInstructions',
+            'recipeYield',
+            'totalTime',
 
-		foreach ( $keys as $key ) {
-			$this->attributes[ $key ] = null;
-		}
-	}
+            // Pluralized form of schema.org supported properties.
+            'recipeCategories',
+            'recipeCuisines',
+            'recipeIngredients',
+        ];
 
-	public function __get( $key ) {
-		if ( array_key_exists( $key, $this->attributes ) ) {
-			return $this->attributes[ $key ];
-		}
+        foreach ($keys as $key) {
+            $this->properties[ $key ] = null;
+        }
+    }
 
-		return null;
-	}
+    public function setCookTime($value)
+    {
+        $this->setDuration('cookTime', $value);
+    }
 
-	public function __set( $key, $value ) {
-		if ( array_key_exists( $key, $this->attributes ) ) {
-			$this->attributes[ $key ] = $value;
-		}
-	}
+    public function setCookingMethod($value)
+    {
+        $this->setText('cookingMethod', $value);
+    }
+
+    public function setPrepTime($value)
+    {
+        $this->setDuration('prepTime', $value);
+    }
+
+    public function setRecipeCategories(array $value)
+    {
+        $this->setTextList('recipeCategories', $value);
+    }
+
+    public function setRecipeCuisines(array $value)
+    {
+        $this->setTextList('recipeCuisines', $value);
+    }
+
+    public function setRecipeIngredients(array $value)
+    {
+        $this->setGroupList('recipeIngredients', $value);
+    }
+
+    public function setRecipeInstructions(array $value)
+    {
+        $this->setGroupList('recipeInstructions', $value);
+    }
+
+    public function setRecipeYield($value)
+    {
+        $this->setText('recipeYield', $value);
+    }
+
+    public function setTotalTime($value)
+    {
+        $this->setDuration('totalTime', $value);
+    }
+
+    /**
+     * Return totalTime if set, otherwise calculate from prep + cook times.
+     *
+     * @return \DateInterval
+     */
+    public function totalTime()
+    {
+        if (! is_null($this->properties['totalTime'])) {
+            return $this->properties['totalTime'];
+        }
+
+        $now = new \DateTime('now');
+        $later = clone $now;
+
+        if (! is_null($this->properties['cookTime'])) {
+            $later->add($this->properties['cookTime']);
+        }
+
+        if (! is_null($this->properties['prepTime'])) {
+            $later->add($this->properties['prepTime']);
+        }
+
+        if ($later == $now) {
+            return null;
+        }
+
+        return $now->diff($later);
+    }
+
+    /**
+     * Throw an exception if array contains anything but string values.
+     *
+     * @param  array  $value [description]
+     *
+     * @throws \InvalidArgumentException When non-string entries are found.
+     */
+    protected function assertArrayOfStrings(array $value)
+    {
+        array_walk($value, function ($v, $k) {
+            if (! is_string($v)) {
+                throw new \InvalidArgumentException(sprintf(
+                    'All entries in the value property are required to be string, %s found at index %s',
+                    gettype($v),
+                    $k
+                ));
+            }
+        });
+    }
+
+    /**
+     * Set a property to a \DateInterval value.
+     *
+     * @param string        $key   Valid recipe property name.
+     * @param \DateInterval $value Duration to set.
+     */
+    protected function setDuration($key, \DateInterval $value)
+    {
+        $this->assertValidProperty($key);
+
+        $this->properties[ $key ] = $value;
+    }
+
+    /**
+     * Set a property to an array of arrays each with title and data.
+     *
+     * @param string $key   Valid recipe property name.
+     * @param array  $value Group list.
+     *
+     * @throws \InvalidArgumentException When entry in $value is not array.
+     * @throws \InvalidArgumentException When entry does not have title or data.
+     * @throws \InvalidArgumentException When title or data ar of wrong type.
+     */
+    protected function setGroupList($key, array $value)
+    {
+        $this->assertValidProperty($key);
+
+        array_walk($value, function ($v, $k) {
+            if (! is_array($v)) {
+                throw new \InvalidArgumentException(sprintf(
+                    'Group list values must be array, %s found',
+                    gettype($v)
+                ));
+            }
+
+            if (! isset($v['title']) || ! isset($v['data'])) {
+                throw new \InvalidArgumentException(
+                    'Group list values must contain a title and a data entry'
+                );
+            }
+
+            if (! is_string($v['title']) || ! is_array($v['data'])) {
+                throw new \InvalidArgumentException(
+                    'Group list values must contain a title (string) and data (array)'
+                );
+            }
+
+            $this->assertArrayOfStrings($v['data']);
+        });
+
+        $this->properties[ $key ] = $value;
+    }
+
+    /**
+     * Set a property to an array of strings.
+     *
+     * @param string $key   Valid recipe property name.
+     * @param array  $value Array of strings to set.
+     */
+    protected function setTextList($key, array $value)
+    {
+        $this->assertValidProperty($key);
+        $this->assertArrayOfStrings($value);
+
+        $this->properties[ $key ] = $value;
+    }
 }
---
+++ src/Schema/Thing.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Schema.org Thing. Not complete, tailored around recipes. Also modified
+ * slightly to better match implementations found in the wild.
+ */
+
+namespace SSNepenthe\RecipeParser\Schema;
+
+/**
+ * This class represents a Thing object as defined by schema.org. It has
+ * been paired down significantly to only include properties desirable on the
+ * Recipe sub-object.
+ *
+ * @link https://schema.org/Thing
+ */
+class Thing
+{
+    protected $properties = [];
+
+    public function __construct()
+    {
+        $keys = [
+            'description',
+            'image',
+            'name',
+            'url',
+        ];
+
+        foreach ($keys as $key) {
+            $this->properties[ $key ] = null;
+        }
+    }
+
+    public function __get($key)
+    {
+        if (! array_key_exists($key, $this->properties)) {
+            return null;
+        }
+
+        if (method_exists($this, $key)) {
+            return $this->$key();
+        }
+
+        return $this->properties[ $key ];
+    }
+
+    public function getKeys()
+    {
+        return array_keys($this->properties);
+    }
+
+    public function setDescription($value)
+    {
+        $this->setText('description', $value);
+    }
+
+    public function setImage($value)
+    {
+        $this->setText('image', $value);
+    }
+
+    public function setName($value)
+    {
+        $this->setText('name', $value);
+    }
+
+    public function setUrl($value)
+    {
+        $this->setText('url', $value);
+    }
+
+    /**
+     * Throw an exception if provided an invalid property name.
+     *
+     * @param  string $key Property name to check validity of.
+     *
+     * @throws \InvalidArgumentException When key is not a string.
+     * @throws \RuntimeException When key is not a valid property.
+     */
+    protected function assertValidProperty($key)
+    {
+        if (! is_string($key)) {
+            throw new \InvalidArgumentException(sprintf(
+                'The key property is required to be string, was: %s',
+                gettype($value)
+            ));
+        }
+
+        if (! array_key_exists($key, $this->properties)) {
+            throw new \RuntimeException(sprintf(
+                '%s is not a valid property of %s',
+                $key,
+                __CLASS__
+            ));
+        }
+    }
+
+    /**
+     * Set a property ro a string value.
+     *
+     * @param string $key   A valid recipe property name.
+     * @param string $value The string value to set.
+     *
+     * @throws \InvalidArgumentException When value is not a string.
+     */
+    protected function setText($key, $value)
+    {
+        $this->assertValidProperty($key);
+
+        if (! is_string($value)) {
+            throw new \InvalidArgumentException(sprintf(
+                'The value property is required to be string, was: %s',
+                gettype($value)
+            ));
+        }
+
+        $this->properties[ $key ] = $value;
+    }
+}
