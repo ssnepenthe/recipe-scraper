458b6c12d024862921f3619f5a1b31aa4a9e4fc2
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-26 13:03:14 -0700

    Update cache and HTTP for new interfaces.

--- src/Cache/DoctrineFSCache.php
+++ src/Cache/DoctrineFSCache.php
@@ -3,32 +3,37 @@
 namespace SSNepenthe\RecipeParser\Cache;
 
 use Doctrine\Common\Cache\FilesystemCache;
-use SSNepenthe\RecipeParser\Interfaces\CacheInterface;
+use SSNepenthe\RecipeParser\Interfaces\CacheProvider;
 
-class DoctrineFSCache implements CacheInterface {
-	protected $cache;
+class DoctrineFSCache implements Cache
+{
+    protected $cache;
 
-	public function __construct( $cache_dir ) {
-		$cache_dir = realpath( $cache_dir );
+    public function __construct($cache_dir)
+    {
+        $cache_dir = realpath($cache_dir);
 
-		if ( ! is_dir( $cache_dir ) || ! is_writable( $cache_dir ) ) {
-			throw new \RuntimeException(
-				'Supplied cache directory is not valid or not writable.'
-			);
-		}
+        if (! is_dir($cache_dir) || ! is_writable($cache_dir)) {
+            throw new \RuntimeException(
+                'Supplied cache directory is not valid or not writable.'
+            );
+        }
 
-		$this->cache = new FilesystemCache( $cache_dir );
-	}
+        $this->cache = new FilesystemCache($cache_dir);
+    }
 
-	public function fetch( $id ) {
-		return $this->cache->fetch( $id );
-	}
+    public function fetch($id)
+    {
+        return $this->cache->fetch($id);
+    }
 
-	public function flush() {
-		return $this->cache->flushAll();
-	}
+    public function flush()
+    {
+        return $this->cache->flushAll();
+    }
 
-	public function save( $id, $data, $lifetime = 60 * 60 * 24 ) {
-		return $this->cache->save( $id, $data, $lifetime );
-	}
+    public function save($id, $data, $lifetime = 60 * 60 * 24)
+    {
+        return $this->cache->save($id, $data, $lifetime);
+    }
 }
