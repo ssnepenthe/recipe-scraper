ff468e67dfff3fbfa7671e8205fbf9b149947308
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-27 14:18:47 -0700

    First pass at support for pauladeen.com.

--- src/Parsers/PaulaDeenCom.php
+++ src/Parsers/PaulaDeenCom.php
@@ -3,16 +3,29 @@
 namespace SSNepenthe\RecipeParser\Parsers;
 
 /**
- * Need to test yield more extensively.
+ * @todo Has videos if we want them. See http://www.pauladeen.com/chicken-chili-stew.
+ *       More thorough testing on url - there are two canonical links per page.
  */
-class PaulaDeenCom extends SchemaOrg {
-	protected function set_paths() {
-		parent::set_paths();
+class PaulaDeenCom extends SchemaOrg
+{
+	protected function configure()
+	{
+		parent::configure();
 
-		$this->paths['image'][0] = './/img[@class="base-product-image"]';
-		$this->paths['recipe_ingredient'][0] = './/*[@class="detail-box ingredients"]/ul/li';
-		$this->paths['recipe_instructions'][0] = './/*[@class="detail-box preparation"]/p';
-		$this->paths['recipe_yield'][0] = './/div[@class="recipe-info"]/div[4]/span[@class="data"]';
-		$this->paths['url'][0] = './/*[@rel="canonical"]';
+		$this->config['description']['selector'] = 'meta[name="description"]';
+		$this->config['recipeIngredients']['selector'] = '[itemtype*="schema.org/Recipe"] .ingredients li';
+		$this->config['recipeInstructions']['selector'] = '[itemtype*="schema.org/Recipe"] .preparation p';
+		$this->config['url']['selector'] = 'link[rel="canonical"]:last-of-type';
+	}
+
+	/**
+	 * Gets the first text node child and leaves the span behind.
+	 *
+	 * @param  DOMNodeList $nodelist List of node retrieved from DOM.
+	 *
+	 * @return string
+	 */
+	protected function fetchName( $nodelist ) {
+		return $nodelist->item(0)->childNodes->item(0)->nodeValue;
 	}
 }
