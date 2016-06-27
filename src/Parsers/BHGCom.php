b959da6927033e5ba510276dc0a7bc757279508c
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-26 19:39:07 -0700

    First pass at support for bhg.com.

--- src/Parsers/BHGCom.php
+++ src/Parsers/BHGCom.php
@@ -3,16 +3,19 @@
 namespace SSNepenthe\RecipeParser\Parsers;
 
 /**
- * Calculate total time from prep and cook times
+ * FYI there is a JSON API if you send header 'Accept: application/json'.
  *
- * They appear to have a JSON api if you send header 'Accept: application/json'.
- * Definitely worth exploring further...
+ * @todo  They have nutrition info if we want it.
+ *        Calculate total time from prep + cook times.
  */
-class BHGCom extends SchemaOrg {
-	protected function set_paths() {
-		parent::set_paths();
+class BHGCom extends SchemaOrg
+{
+    protected function configure()
+    {
+        parent::configure();
 
-		$this->paths['image'][0] = './/*[@itemprop="thumbnail"]';
-		$this->paths['name'][0] = './/h1[@itemprop="name"]';
-	}
+        $this->config['image']['selector'] = '[itemtype*="schema.org/Recipe"] [itemprop="thumbnail"]';
+        $this->config['name']['selector'] = 'h1[itemprop="name"]';
+        $this->config['recipeInstructions']['selector'] = '[itemprop="recipeInstructions"] li';
+    }
 }
