5f3648cfc9853cfaae5f1f994adc1ddb0f6ffabe
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-27 14:35:06 -0700

    First pass at support for tablespoon.com.

--- src/Parsers/TablespoonCom.php
+++ src/Parsers/TablespoonCom.php
@@ -3,18 +3,21 @@
 namespace SSNepenthe\RecipeParser\Parsers;
 
 /**
- * Prep time is only accessible via a javascript object...
- *
- * image: would rather use @class="recipePartRecipeImage" for bigger image but is inconsistent.
- *
- * Consider notes: labeled tips at http://www.tablespoon.com/recipes/do-ahead-breakfast-bake-recipe/2/
+ * @todo Has multiple images we can grab if we want. See http://www.tablespoon.com/recipes/copycat-in-n-out-burger-double-cheeseburger/ea7f1d77-0fbf-448d-acc4-597d8f8fccec#
+ *       Verify consistency of .recipePartRecipeImage across multiple recipes.
+ *       No great way to select prep and cook times - need to revisit.
+ *       They seem to have notes/tips - see link above or http://www.tablespoon.com/recipes/do-ahead-breakfast-bake-recipe/2/ - might require javascript.
  */
-class TablespoonCom extends SchemaOrg {
-	protected function set_paths() {
-		parent::set_paths();
+class TablespoonCom extends SchemaOrg
+{
+	protected function configure()
+	{
+		parent::configure();
 
-		$this->paths['recipe_category'][0] = './/*[@class="tags"]/ul/li/a';
-		$this->paths['recipe_instructions'][0] = './/*[@itemprop="recipeInstructions"]/span[@class="recipePartStepDescription"]';
-		$this->paths['url'][0] = './/*[@rel="canonical"]';
+		$this->config['author']['locations'] = ['nodeValue'];
+		$this->config['description']['selector'] = 'meta[name="description"]';
+		$this->config['image']['selector'] = '.recipePartRecipeImage img';
+		$this->config['recipeIngredients']['selector'] = '.recipePartIngredientGroup h2, .recipePartIngredientGroup dl';
+		$this->config['url']['selector'] = 'link[rel="canonical"]';
 	}
 }
