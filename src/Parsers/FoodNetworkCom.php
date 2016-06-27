9009df7e16c99bde9daebc68204f30654492ccf1
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-27 14:03:20 -0700

    First pass at support for foodnetwork.com.

--- src/Parsers/FoodNetworkCom.php
+++ src/Parsers/FoodNetworkCom.php
@@ -2,13 +2,57 @@
 
 namespace SSNepenthe\RecipeParser\Parsers;
 
-/**
- * Ideally we should leave off the last element in instructions.
- */
-class FoodNetworkCom extends SchemaOrg {
-	protected function set_paths() {
-		parent::set_paths();
+use SSNepenthe\RecipeParser\Util\Format;
+use SSNepenthe\RecipeParser\Util\Normalize;
 
-		$this->paths['recipe_instructions'][0] = './/*[@itemprop="recipeInstructions"]/p';
-	}
+/**
+ * @todo Potentially more categories available with javascript support.
+ */
+class FoodNetworkCom extends SchemaOrg
+{
+    protected function configure()
+    {
+        parent::configure();
+
+        $this->config['author']['selector'] = '[itemprop="author"] [itemprop="name"]';
+
+        // I don't want the description they actually provide.
+        $this->config['description']['selector'] = '[itemprop="fakedescription"]';
+
+        $this->config['image']['selector'] = '.photo-video [itemprop="image"]';
+        $this->config['recipeIngredients']['selector'] = '.ingredients li';
+        $this->config['recipeInstructions']['selector'] = '.recipe-directions-list li';
+    }
+
+    protected function normalizeRecipeInstructions(array $instructions)
+    {
+        $instructions = array_map([Normalize::class, 'whiteSpace'], $instructions);
+
+        $instructions = $this->stripCopyrightFromInstructions($instructions);
+        $instructions = $this->stripNutritionFromInstructions($instructions);
+        $instructions = $this->stripPhotoCreditFromInstructions($instructions);
+
+        return $instructions;
+    }
+
+    protected function stripPhotoCreditFromInstructions(array $instructions)
+    {
+        return array_filter($instructions, function($value) {
+            return 1 !== preg_match('/Photographs? by/', $value);
+        });
+    }
+
+    protected function stripNutritionFromInstructions(array $instructions)
+    {
+        return array_filter($instructions, function($value) {
+            return false === strpos($value, 'Per serving: ');
+        });
+    }
+
+    protected function stripCopyrightFromInstructions(array $instructions)
+    {
+        return array_filter($instructions, function($value) {
+            return false === strpos($value, 'Copyright');
+        });
+    }
 }
