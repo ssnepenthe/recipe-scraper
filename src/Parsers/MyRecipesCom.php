f86c94c9bea4b5d730773ac30cc360c4ea65c8a1
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-27 14:15:39 -0700

    First pass at support for myrecipes.com.

--- src/Parsers/MyRecipesCom.php
+++ src/Parsers/MyRecipesCom.php
@@ -2,41 +2,22 @@
 
 namespace SSNepenthe\RecipeParser\Parsers;
 
-use DateInterval;
-
 /**
- * Need to revisit times...
+ * @todo Need to revisit times.
+ *       Can we pull a larger image from somewhere?
+ *       Cuisine exists in JS vars, haven't found it anywhere in HTML.
+ *       Yield includes serving size...
  */
-class MyRecipesCom extends DataVocabularyOrg {
-	protected function set_paths() {
-		parent::set_paths();
+class MyRecipesCom extends SchemaOrg
+{
+	protected function configure()
+	{
+		parent::configure();
 
-		$this->paths['author'][0] = './/*[@class="field-author"]';
-		$this->paths['publisher'][0] = './/*[@class="field-sponsor"]';
-		$this->paths['recipe_instructions'][0] = './/*[@itemprop="instructions"]//p';
-	}
-
-	protected function cook_time() {
-		$time = $this->get_single_item( $this->paths['cook_time'] );
-		$this->recipe->cook_time = $time ? DateInterval::createFromDateString( $time ) : null;
-	}
-
-	protected function prep_time() {
-		$time = $this->get_single_item( $this->paths['prep_time'] );
-		$this->recipe->prep_time = $time ? DateInterval::createFromDateString( $time ) : null;
-	}
-
-	protected function recipe_instructions() {
-		parent::recipe_instructions();
-
-		$this->recipe->recipe_instructions = array_map(
-			[ $this, 'strip_leading_numbers' ],
-			$this->recipe->recipe_instructions
-		);
-	}
-
-	protected function total_time() {
-		$time = $this->get_single_item( $this->paths['total_time'] );
-		$this->recipe->total_time = $time ? DateInterval::createFromDateString( $time ) : null;
+		$this->config['author']['selector'] = '[itemprop="author"] .field-author';
+		$this->config['publisher']['selector'] = '.field-sponsor';
+		$this->config['recipeInstructions']['selector'] = '[itemprop="recipeInstructions"] p';
+		$this->config['totalTime']['selector'] = '.recipe-totaltime-duration';
+		$this->config['url']['selector'] = '[rel="canonical"]';
 	}
 }
