97a0da1b6d03866872867c7ba547e4528869e342
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-27 17:32:03 -0700

    Update parse command to use latest API.

--- src/Console/Command/ParseCommand.php
+++ src/Console/Command/ParseCommand.php
@@ -2,118 +2,62 @@
 
 namespace SSNepenthe\RecipeParser\Console\Command;
 
-use SSNepenthe\RecipeParser\Cache\DoctrineFSCache as Cache;
-use SSNepenthe\RecipeParser\Http\CurlClient as Client;
-use SSNepenthe\RecipeParser\RecipeParser;
+use SSNepenthe\RecipeParser\ParserLocator;
+use SSNepenthe\RecipeParser\Http\CurlClient;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use SSNepenthe\RecipeParser\Cache\DoctrineFSCache as Cache;
 
-class ParseCommand extends Command {
-	public function configure() {
-		$this->setName( 'parse' )
-			->setDescription( 'Parse a recipe from the command line.' )
-			->addArgument(
-				'URL',
-				InputArgument::REQUIRED,
-				'URL of the recipe you want to parse.'
-			);
-	}
+class ParseCommand extends Command
+{
+    public function configure()
+    {
+        $this->setName('parse')
+            ->setDescription('Parse a recipe from the command line.')
+            ->addArgument(
+                'URL',
+                InputArgument::REQUIRED,
+                'URL of the recipe you want to parse.'
+            );
+    }
 
-	public function execute( InputInterface $input, OutputInterface $output ) {
-		$url = filter_var( $input->getArgument( 'URL' ), FILTER_SANITIZE_URL );
-		$cache = new Cache(
-			sprintf( '%s/.cache', dirname( dirname( dirname( ( __DIR__ ) ) ) ) )
-		);
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $url = filter_var($input->getArgument('URL'), FILTER_SANITIZE_URL);
 
-		$parser = new RecipeParser( new Client, $cache );
+        /**
+         * @todo Check if we can write to system tmp dir first, fallback to .cache.
+         *       Think about sharing cache config beteen commands.
+         */
+        $cache = new Cache(
+            sprintf('%s/.cache', dirname(dirname(dirname(( __DIR__ )))))
+        );
 
-		if ( ! $output->isQuiet() ) {
-			$io = new SymfonyStyle( $input, $output );
-			$recipe = $parser->parse( $url );
+        $http = new CurlClient;
 
-			$io->title(
-				$recipe->name ? $recipe->name : 'Untitled Recipe'
-			);
+        if (! $html = $cache->fetch($url)) {
+            $html = $http->get($url);
 
-			$io->section( 'Meta' );
+            $cache->save($url, $html);
+        }
 
-			$io->text( [
-				$recipe->author ?
-					sprintf( 'Author: %s.', $recipe->author ) :
-					'No author found.',
-				$recipe->publisher ?
-					sprintf( 'Publisher: %s.', $recipe->publisher ) :
-					'No publisher found.',
-			] );
+        $locator = new ParserLocator($url, $html);
+        $located = $locator->locate();
 
-			$io->section( 'URLs' );
+        if (! $output->isQuiet()) {
+            $parser = new $located($html);
 
-			$io->text( [
-				$recipe->image ?
-					sprintf( 'Image: %s.', $recipe->image ) :
-					'No image found.',
-				$recipe->url ?
-					sprintf( 'Recipe: %s', $recipe->url ) :
-					'No URL found.',
-			] );
+            $recipe = $parser->parse($url);
 
-			$io->section( 'Description' );
-
-			$io->text(
-				$recipe->description ?
-					$recipe->description :
-					'No description found.'
-			);
-
-			$io->section( 'Categories' );
-
-			$io->listing(
-				$recipe->recipe_categories ?
-					$recipe->recipe_categories :
-					[ 'No categories found' ]
-			);
-
-			$io->section( 'Yield' );
-
-			$io->text(
-				$recipe->recipe_yield ?
-					$recipe->recipe_yield :
-					'No yield found.'
-			);
-
-			$io->section( 'Timings' );
-			$f = '%h hours, %i minutes and %s seconds';
-
-			$io->text( [
-				$recipe->cook_time ?
-					sprintf( 'Cook Time: %s.', $recipe->cook_time->format( $f ) ) :
-					'No cook time found.',
-				$recipe->prep_time ?
-					sprintf( 'Prep Time: %s.', $recipe->prep_time->format( $f ) ) :
-					'No prep time found.',
-				$recipe->total_time ?
-					sprintf( 'Total Time: %s.', $recipe->total_time->format( $f ) ) :
-					'No total time found.',
-			] );
-
-			$io->section( 'Ingredients' );
-
-			$io->listing(
-				$recipe->recipe_ingredients ?
-					$recipe->recipe_ingredients :
-					[ 'No ingredients found.' ]
-			);
-
-			$io->section( 'Instructions' );
-
-			$io->listing(
-				$recipe->recipe_instructions ?
-					$recipe->recipe_instructions :
-					[ 'No instructions found.' ]
-			);
-		}
-	}
+            /**
+             * @todo Better looking output.
+             *       Also, dump() is from symfony var-dumper which is not an
+             *       actual dependency of this project - it is pulled in with
+             *       psysh. Psysh is a dev-only dependency.
+             */
+            dump($recipe);
+        }
+    }
 }
