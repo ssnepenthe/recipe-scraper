df41572462ac32de11b1f17b7ca50ab42bdce9b3
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-27 17:12:27 -0700

    First pass at support for thepioneerwoman.com.

--- src/Parsers/ThePioneerWomanCom.php
+++ src/Parsers/ThePioneerWomanCom.php
@@ -5,35 +5,49 @@ namespace SSNepenthe\RecipeParser\Parsers;
 use DateInterval;
 
 /**
- * think about handling ingredient and instruction groups
- *
- * think about adding notes
+ * @todo More testing on description.
+ *       Image size appears to be modified after page load. Can potentially get
+ *       original image by grabbing the parent <a> of the selected <img>.
+ *       Post has author at [rel="author"], but maybe not great for recipe.
  */
-class ThePioneerWomanCom extends SchemaOrg {
-	protected function set_paths() {
-		parent::set_paths();
+class ThePioneerWomanCom extends SchemaOrg
+{
+	protected function configure()
+	{
+		parent::configure();
 
-		$this->paths['image'][0] = './/div[@class="recipe-summary-thumbnail"]/img';
-		$this->paths['recipe_instructions'][0] = './/*[@itemprop="recipeInstructions"]';
-		$this->paths['url'][0] = './/link[@rel="canonical"]';
+		// Get first image in .entry-content.
+		$this->config['image']['selector'] = '.entry-content img:first-child';
+		$this->config['recipeIngredients']['selector'] = '[itemprop="ingredients"]';
+		$this->config['recipeInstructions']['selector'] = '[itemprop="recipeInstructions"]';
+		$this->config['url']['selector'] = '[rel="canonical"]';
 	}
 
-	protected function cook_time() {
-		$time = $this->get_single_item( $this->paths['cook_time'] );
-		$this->recipe->cook_time = $time ? DateInterval::createFromDateString( $time ) : null;
+	protected function fetchRecipeInstructions(\DOMNodeList $nodes) {
+		return $this->itemFromNodeList($nodes, 'recipeInstructions');
 	}
 
-	protected function prep_time() {
-		$time = $this->get_single_item( $this->paths['prep_time'] );
-		$this->recipe->prep_time = $time ? DateInterval::createFromDateString( $time ) : null;
+	protected function formatCookTime($value) {
+		if (! $value) {
+			return $value;
+		}
+
+		return \DateInterval::createFromDateString($value);
 	}
 
-	protected function recipe_instructions() {
-		$this->recipe->recipe_instructions = $this->get_list_from_single_item( $this->paths['recipe_instructions'] );
+	protected function formatPrepTime($value) {
+		if (! $value) {
+			return $value;
+		}
+
+		return \DateInterval::createFromDateString($value);
 	}
 
-	protected function total_time() {
-		$time = $this->get_single_item( $this->paths['total_time'] );
-		$this->recipe->total_time = $time ? DateInterval::createFromDateString( $time ) : null;
+	protected function formatTotalTime($value) {
+		if (! $value) {
+			return $value;
+		}
+
+		return \DateInterval::createFromDateString($value);
 	}
 }
