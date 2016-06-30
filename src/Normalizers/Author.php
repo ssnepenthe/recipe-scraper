92b2594643a00062dfccfab8a204efbf7fe1b2c4
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-30 15:30:36 -0700

    First pass as author normalizer.

---
+++ src/Normalizers/Author.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SSNepenthe\RecipeScraper\Normalizers;
+
+use SSNepenthe\RecipeScraper\Interfaces\Normalizer;
+
+class Author implements Normalizer {
+	public function normalize(array $value) {
+		return array_map(function($v) {
+			return trim(str_ireplace('By', '', $v), ": \t\n\r\0\x0B");
+		}, $value);
+	}
+}
