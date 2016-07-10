385380271be62d8cda958aad611a73f61162218c
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-07-09 22:18:06 -0700

    First pass at recipe instructions normalizer.

---
+++ src/Normalizers/RecipeInstructions.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SSNepenthe\RecipeScraper\Normalizers;
+
+use SSNepenthe\RecipeScraper\Interfaces\Normalizer;
+
+class RecipeInstructions implements Normalizer {
+	public function normalize(array $values) {
+		return array_filter($values, function($v) {
+			if (1 === preg_match('/Photographs? by/', $v)) {
+				return false;
+			}
+
+			if (false !== strpos($v, 'Per serving: ')) {
+				return false;
+			}
+
+			if (false !== strpos($v, 'Copyright')) {
+				return false;
+			}
+
+			return true;
+		});
+	}
+}
