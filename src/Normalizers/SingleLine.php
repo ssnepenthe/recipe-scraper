29b0c0f95c12d893343644cbf533739e92de1512
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-07-09 22:18:24 -0700

    First pass at single line normalizer.

---
+++ src/Normalizers/SingleLine.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SSNepenthe\RecipeScraper\Normalizers;
+
+use SSNepenthe\RecipeScraper\Interfaces\Normalizer;
+
+class SingleLine implements Normalizer {
+	public function normalize(array $values) {
+		return array_map(function($v) {
+			return trim(str_replace(PHP_EOL, ' ', $v));
+		}, $values);
+	}
+}
