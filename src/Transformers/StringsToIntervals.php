fdd8a614a93c3fd89bdf9091c28cb31ac43c3a7e
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-07-09 22:22:51 -0700

    First pass at string to interval transformer.

---
+++ src/Transformers/StringsToIntervals.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SSNepenthe\RecipeScraper\Transformers;
+
+use SSNepenthe\RecipeScraper\Interfaces\Transformer;
+
+class StringsToIntervals implements Transformer
+{
+	public function transform(array $values)
+	{
+		$values = array_values(array_filter(array_map(function($v) {
+			try {
+				$interval = new \DateInterval($v);
+			} catch (\Exception $e) {
+				$interval = \DateInterval::createFromDateString($v);
+			}
+
+			// Did DateInterval::createFromDateString create an empty interval?
+			if (
+				! $interval->y && ! $interval->m && ! $interval->d &&
+				! $interval->h && ! $interval->i && ! $interval->s
+			) {
+				$interval = null;
+			}
+
+			return $interval;
+		}, $values)));
+
+		return $values;
+	}
+}
