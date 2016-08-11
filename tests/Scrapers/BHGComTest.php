4064bf9910c5ca215d3d09d53668e276fbb847e3
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-08-10 18:03:20 -0700

    Add additional tests for bhg.com.

--- tests/Scrapers/BHGComTest.php
+++ tests/Scrapers/BHGComTest.php
@@ -3,6 +3,9 @@
 use SSNepenthe\RecipeScraper\Scrapers\BHGCom;
 use SSNepenthe\RecipeScraper\Schema\Recipe;
 
+/**
+ * @todo Haven't been able to find recipes with ingredient groups.
+ */
 class BHGComTest extends CachedHTTPTestCase
 {
     public function test_parse_a_standard_recipe()
@@ -64,6 +67,73 @@ class BHGComTest extends CachedHTTPTestCase
         );
         $scraper = new BHGCom($crawler);
 
-        $this->assertEquals($recipe, $scraper->scrape());
+        $scraped = $scraper->scrape();
+
+        $this->assertEquals($recipe, $scraped);
+        $this->assertEquals(new DateInterval('PT4H25M'), $scraped->totalTime);
+    }
+
+    public function test_parse_another_standard_recipe()
+    {
+        $recipe = new Recipe;
+        $recipe->setAuthor('Better Homes and Gardens');
+        $recipe->setDescription('Flavors meld beautifully when you roast the apples alongside the seasoned pork in this easy, low-calorie dinner recipe.');
+        $recipe->setImage('http://images.meredith.com/content/dam/bhg/Images/recipe/43/R137745.jpg.rendition.largest.ss.jpg');
+        $recipe->setName('Roasted Pork with Apples');
+        $recipe->setPrepTime(new DateInterval('PT15M'));
+        $recipe->setRecipeCategories([
+            'Diabetic Recipes',
+            'Dinner Recipes',
+            'Fruit Salad Recipes',
+            'Healthy Breakfast Recipes',
+            'Healthy Dinner Recipes',
+            'Healthy Lunch Recipes',
+            'Healthy Recipes',
+            'Heart-Healthy Recipes',
+            'Low Fat Recipes',
+            'Pork Recipes',
+            'Quick and Easy Healthy Recipes',
+            'Quick and Easy Recipes',
+            'Seasonal Recipes',
+        ]);
+        $recipe->setRecipeIngredients([
+            [
+                'title' => '',
+                'data' => [
+                    '1 teaspoon snipped fresh sage or 1/2 teaspoon dried sage, crushed',
+                    '1/4 teaspoon salt',
+                    '1/4 teaspoon coarsely ground black pepper',
+                    '1 1 pound pork tenderloin',
+                    '1 tablespoon canola oil',
+                    '1 medium red onion, cut into thin wedges',
+                    '3 medium (1 pound) cooking apples (such as Granny Smith or Jonathan), cored and cut into 1/2-inch thick wedges',
+                    '2/3 cup apple juice or apple cider',
+                    'Fresh sage sprigs (optional)',
+                ],
+            ]
+        ]);
+        $recipe->setRecipeInstructions([
+            [
+                'title' => '',
+                'data' => [
+                    'In a small bowl, combine the snipped sage, salt, and pepper; rub on all sides of tenderloin. In a large skillet brown tenderloin in hot oil over medium heat, turning to brown all sides.',
+                    'Transfer pork to a shallow roasting pan. Add onion to pan around pork. Roast, uncovered, at 425 degrees F for 10 minutes. Stir in apples; roast for 10 to 15 minutes more or until pork internal temperature registers 155 degrees F on instant read thermometer and juices run clear.',
+                    'Transfer pork and apple mixture to serving platter; cover with foil. Let stand for 10 minutes before slicing (temperature of the meat will rise 5 degrees F while it stands).',
+                    'In a small saucepan bring apple juice to a boil; simmer gently, uncovered, for 8 to 10 minutes or until reduced to 1/4 to 1/3 cup. Drizzle over meat and apple mixture. If desired, garnish with additional sage sprigs.',
+                ],
+            ]
+        ]);
+        $recipe->setRecipeYield('4');
+        $recipe->setUrl('http://www.bhg.com/recipe/pork/roasted-pork-with-apples/');
+
+        $crawler = $this->client->request(
+            'GET',
+            'http://www.bhg.com/recipe/pork/roasted-pork-with-apples/'
+        );
+        $scraper = new BHGCom($crawler);
+
+        $scraped = $scraper->scrape();
+
+        $this->assertEquals($recipe, $scraped);
     }
 }
