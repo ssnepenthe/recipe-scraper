84818f29c3c99a92166d304038780ccb6d8fb8c4
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-05-31 15:44:42 -0700

    First pass at allrecipes.com results

---
+++ tests/data/results/allrecipes.com/recipe-139917-joses-shrimp-ceviche.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'author' => 'carrielee',
+    'categories' => [],
+    'cookingMethod' => '',
+    'cookTime' => 'PT10M',
+    'cuisines' => [],
+    'description' => 'I\'ve looked all over the net and haven\'t found a shrimp ceviche quite like this one!   My friends absolutely love it and beg me for the recipe!  You can always double it for larger parties--it goes FAST! Serve as a dip with tortilla chips or as a topping on a tostada spread with mayo. The fearless palate might like this with hot sauce.',
+    'image' => 'http://images.media-allrecipes.com/userphotos/560x315/1364063.jpg',
+    'ingredients' => [
+        '1 pound peeled and deveined medium shrimp',
+        '1 cup fresh lime juice',
+        '10 plum tomatoes, diced',
+        '1 large yellow onion, diced',
+        '1 jalapeno pepper, seeded and minced, or to taste',
+        '2 avocados, diced (optional)',
+        '2 ribs celery, diced (optional)',
+        'chopped fresh cilantro to taste',
+        'salt and pepper to taste',
+    ],
+    'instructions' => [
+        'Place shrimp in a glass bowl and cover with lime juice to marinate (or \'cook\') for about 10 minutes, or until they turn pink and opaque. Meanwhile, place the plum tomatoes, onion and jalapeno (and avocados and celery, if using) in a large, non-reactive (stainless steel, glass or plastic) bowl.',
+        'Remove shrimp from lime juice, reserving juice. Dice shrimp and add to the bowl of vegetables. Pour in the remaining lime juice marinade. Add cilantro and salt and pepper to taste. Toss gently to mix.'
+    ],
+    'name' => 'Jose\'s Shrimp Ceviche',
+    'prepTime' => 'PT45M',
+    'publisher' => '',
+    'totalTime' => 'PT55M',
+    'url' => 'http://allrecipes.com/recipe/139917/joses-shrimp-ceviche/',
+    'yield' => '20',
+];
---
+++ tests/data/results/allrecipes.com/recipe-8652-garlic-chicken.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'author' => 'Carol',
+    'categories' => [],
+    'cookingMethod' => '',
+    'cookTime' => 'PT35M',
+    'cuisines' => [],
+    'description' => 'This easy, tasty dish is perfect for a weeknight dinner.',
+    'image' => 'http://images.media-allrecipes.com/userphotos/250x250/394412.jpg',
+    'ingredients' => [
+        '1/4 cup olive oil',
+        '2 cloves garlic, crushed',
+        '1/4 cup Italian-seasoned bread crumbs',
+        '1/4 cup grated Parmesan cheese',
+        '4 skinless, boneless chicken breast halves',
+    ],
+    'instructions' => [
+        'Preheat oven to 425 degrees F (220 degrees C).',
+        'Heat olive oil and garlic in a small saucepan over low heat until warmed, 1 to 2 minutes. Transfer garlic and oil to a shallow bowl.',
+        'Combine bread crumbs and Parmesan cheese in a separate shallow bowl.',
+        'Dip chicken breasts in the olive oil-garlic mixture using tongs; transfer to bread crumb mixture and turn to evenly coat. Transfer coated chicken to a shallow baking dish.',
+        'Bake in the preheated oven until no longer pink and juices run clear, 30 to 35 minutes. An instant-read thermometer inserted into the center should read at least 165 degrees F (74 degrees C).',
+    ],
+    'name' => 'Garlic Chicken',
+    'prepTime' => 'PT20M',
+    'publisher' => '',
+    'totalTime' => 'PT55M',
+    'url' => 'http://allrecipes.com/recipe/8652/garlic-chicken/',
+    'yield' => '4',
+];
