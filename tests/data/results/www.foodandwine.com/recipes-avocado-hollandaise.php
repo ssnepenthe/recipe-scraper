a4592b4172954921e3d09597c3b42afeb52110ff
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-01 09:03:44 -0700

    First pass at www.foodandwine.com results

---
+++ tests/data/results/www.foodandwine.com/recipes-avocado-hollandaise.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'author' => 'Kay Chun',
+    'categories' => [
+        'Easter',
+        'Mother\'s Day',
+        'Sauces & Condiments',
+        'Fast',
+        'Gluten-Free',
+        'Healthy',
+        'Vegetarian',
+        'Breakfast',
+        'Brunch',
+    ],
+    'cookingMethod' => '',
+    'cookTime' => '',
+    'cuisines' => [],
+    'description' => 'Luscious, rich and lemony hollandaise gets completely re-imagined here in a light, supremely creamy puree of avocado, lemon juice and olive oil.',
+    'image' => 'http://cdn-image.foodandwine.com/sites/default/files/styles/551x551/public/201402-xl-avocado-hollandaise.jpg?itok=BA_I8MaS',
+    'ingredients' => [
+        '1/2 very ripe medium Hass avocado, peeled and chopped',
+        '2 teaspoons fresh lemon juice',
+        '2 tablespoons extra-virgin olive oil',
+        'Kosher salt',
+        'Freshly ground pepper',
+        'Poached eggs, for serving',
+    ],
+    'instructions' => [
+        'In a blender, combine the avocado and lemon juice with 1/3 cup of hot water. Puree until smooth and light in texture, about 2 minutes, scraping down the side of the bowl occasionally. With the machine on, drizzle in the olive oil and puree until combined. Season with salt and pepper. Serve the hollandaise over poached eggs.',
+    ],
+    'name' => 'Avocado Hollandaise',
+    'prepTime' => '',
+    'publisher' => '',
+    'totalTime' => 'PT10M',
+    'url' => 'http://www.foodandwine.com/recipes/avocado-hollandaise',
+    'yield' => '4',
+];
---
+++ tests/data/results/www.foodandwine.com/recipes-baby-brioches-chicken-salad-and-bacon.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'author' => 'Melissa Rubel Jacobson',
+    'categories' => [
+        'Fall',
+        'Winter',
+        'Cocktail Party',
+        'New Year\'s Eve',
+        'Appetizers/starters',
+        'Fast',
+    ],
+    'cookingMethod' => '',
+    'cookTime' => '',
+    'cuisines' => [],
+    'description' => 'The classic chicken salad can be bolstered with any number of seasonings, from curry powder or mustard to harissa. To turn these two-bite snacks into mini sandwiches, cut the brioche rolls in half, mound the chicken salad, tomato and bacon inside, and secure with a toothpick.',
+    'image' => 'http://cdn-image.foodandwine.com/sites/default/files/styles/551x551/public/200809-r-xl-baby-brioches-with-chicken-salad-and-bacon.jpg?itok=N3fZx-He',
+    'ingredients' => [
+        '4 slices of applewood-smoked bacon',
+        '16 mini brioche rolls (about 2 inches)',
+        '1/3 cup mayonnaise',
+        '1 celery rib, minced',
+        '1/2 small shallot, minced',
+        '2 teaspoons minced flat-leaf parsley, plus 32 large flat-leaf parsley leaves',
+        '1 teaspoon fresh lemon juice',
+        '2 chicken breasts from a rotisserie chicken—skin and bones discarded, chicken cut into 1/3-inch dice',
+        'Kosher salt and freshly ground black pepper',
+        '2 small plum tomatoes, cut into 1/4-inch slices (16 slices)',
+    ],
+    'instructions' => [
+        'Preheat the oven to 325 °. In a large skillet, cook the bacon over moderate heat until browned and crisp all over, about 5 minutes. Transfer the bacon to paper towels to drain, then cut each slice into 4 pieces.',
+        'Using a paring knife, cut a 1-inch round plug out of the top of each brioche roll. Carefully hollow out the rolls. Set the rolls on a baking sheet and warm them in the oven to refresh them, about 5 minutes.',
+        'In a medium bowl, combine the the mayonnaise, celery, shallot, minced parsley and lemon juice. Stir in the diced chicken and season the salad with salt and pepper.',
+        'Spoon 1 tablespoon of the chicken salad into each brioche roll, garnish with parsley leaves and cover with a tomato slice. Top each roll with 1 teaspoon of chicken salad, garnish with the bacon and serve.',
+    ],
+    'name' => 'Baby Brioches with Chicken Salad and Bacon',
+    'prepTime' => '',
+    'publisher' => '',
+    'totalTime' => 'PT25M',
+    'url' => 'http://www.foodandwine.com/recipes/baby-brioches-chicken-salad-and-bacon',
+    'yield' => '16 hors d\'oeuvres',
+];
