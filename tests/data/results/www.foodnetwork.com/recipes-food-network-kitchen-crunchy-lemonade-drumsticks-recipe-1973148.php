949e1cfc96f22997784b2456f925b0fef27119d6
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-01 09:31:28 -0700

    First pass at www.foodnetwork.com results

---
+++ tests/data/results/www.foodnetwork.com/recipes-food-network-kitchen-crunchy-lemonade-drumsticks-recipe-1973148.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'author' => 'Food Network Kitchen',
+    'categories' => [
+        'Chicken',
+        'Easy',
+    ],
+    'cookingMethod' => '',
+    'cookTime' => 'PT1H10M',
+    'cuisines' => [],
+    'description' => '',
+    'image' => 'http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2010/6/8/5/FNM_070110-Weekend-Dinners-026_s4x3.jpg.rend.sni12col.landscape.jpeg',
+    'ingredients' => [
+        '2 tablespoons grated lemon zest',
+        '1/2 cup fresh lemon juice',
+        '3 tablespoons packed light brown sugar',
+        '1/3 cup buttermilk',
+        '12 skin-on chicken drumsticks (3 1/2 to 4 1/4 pounds)',
+        'Kosher salt and freshly ground black pepper',
+        '2 cups panko (Japanese breadcrumbs)',
+        '1 tablespoon chopped fresh thyme',
+        'Pinch of cayenne pepper',
+        '1/4 cup mayonnaise',
+        'Olive-oil cooking spray',
+    ],
+    'instructions' => [
+        'Mix 1 tablespoon lemon zest and the lemon juice in a large bowl. Add 1 cup water and the sugar and whisk to dissolve, then whisk in the buttermilk. Pierce the drumsticks several times with a fork and season with salt and pepper. Toss in the marinade, cover and refrigerate 4 hours, or overnight.',
+        'Preheat the oven to 400 degrees F. Bring the chicken to room temperature. Put the panko, thyme, the remaining 1 tablespoon lemon zest, the cayenne, 1 teaspoon salt, and black pepper to taste in a large resealable plastic bag and shake to mix. Put the mayonnaise in a bowl. Set a rack on a baking sheet and spray with cooking spray.',
+        'Remove the drumsticks from the marinade, dip in the mayonnaise, then drop into the bag and shake to coat; transfer to the rack.',
+        'Mist the chicken with cooking spray. Bake until golden, about 35 minutes; flip and bake until browned and crisp, 35 to 40 more minutes. Cool completely, then pack in an airtight container.',
+    ],
+    'name' => 'Crunchy Lemonade Drumsticks',
+    'prepTime' => 'PT35M',
+    'publisher' => '',
+    'totalTime' => 'PT1H45M',
+    'url' => 'http://www.foodnetwork.com/recipes/food-network-kitchens/crunchy-lemonade-drumsticks-recipe.html',
+    'yield' => '6',
+];
