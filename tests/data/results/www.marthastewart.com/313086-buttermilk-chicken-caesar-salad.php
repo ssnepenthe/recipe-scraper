6d27a0cbe9f7737f9f71a3ddd33e0b13e473e2f9
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-01 11:28:39 -0700

    First pass at www.marthastewart.com results

---
+++ tests/data/results/www.marthastewart.com/313086-buttermilk-chicken-caesar-salad.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'author' => '',
+    'categories' => [],
+    'cookingMethod' => '',
+    'cookTime' => '',
+    'cuisines' => [],
+    'description' => 'Unlike traditional Caesar salad, ours uses no oil in the dressing. Instead, it relies on tangy low-fat buttermilk. Multigrain croutons also help save on calories and splurge on flavor.',
+    'image' => 'http://assets.marthastewart.com/styles/wmax-520-highdpi/d26/med102917_0507_cesearsalad/med102917_0507_cesearsalad_vert.jpg?itok=_s2SNifv',
+    'ingredients' => [
+        '1 1/2 cups low-fat buttermilk',
+        '2 tablespoons fresh lemon juice',
+        '1 garlic clove, pressed through a garlic press',
+        '1/4 cup grated Parmesan cheese',
+        'Coarse salt and ground pepper',
+        '3 boneless, skinless chicken breast halves (6 to 8 ounces each)',
+        '2 slices multigrain bread',
+        '1 tablespoon olive oil',
+        '1/4 cup light mayonnaise',
+        '2 medium heads (1 1/2 pounds total) romaine lettuce, cut into pieces',
+        '1/2 small head radicchio, thinly sliced',
+    ],
+    'instructions' => [
+        'Heat broiler, with rack set 4 inches from heat. Line a rimmed baking sheet with aluminum foil. In a medium bowl, combine buttermilk, lemon juice, garlic, and Parmesan. Season with salt and pepper.',
+        'Place chicken in a resealable plastic bag; reserving 1/2 cup for salad, add buttermilk mixture. Refrigerate chicken at least 15 minutes and up to 1 day.',
+        'Meanwhile, make croutons: Place bread on prepared baking sheet. Brush both sides with oil, and season with salt and pepper. Broil until toasted, 1 to 2 minutes per side; cut into 1-inch pieces (and reserve baking sheet).',
+        'Transfer chicken to baking sheet; discard marinade. Broil until opaque throughout, 14 to 16 minutes. Let rest 5 minutes; thinly slice crosswise. In a large bowl, stir together mayonnaise and reserved buttermilk mixture. Add romaine, radicchio, chicken, and croutons; toss to combine. Serve immediately.',
+    ],
+    'name' => 'Buttermilk Chicken Caesar Salad',
+    'prepTime' => 'PT15M',
+    'publisher' => '',
+    'totalTime' => 'PT40M',
+    'url' => 'http://www.marthastewart.com/313086/buttermilk-chicken-caesar-salad',
+    'yield' => '4',
+];
