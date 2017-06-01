64d5ddbca4215f5642c0bfd154afe2e9c1403215
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-01 11:52:41 -0700

    First pass at www.pillsbury.com results

---
+++ tests/data/results/www.pillsbury.com/recipes-slow-cooker-cheesy-chicken-enchilada-chili-389d56ac-2840-4327-a23b-1303539a7248.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'author' => '',
+    'categories' => [],
+    'cookingMethod' => '',
+    'cookTime' => '',
+    'cuisines' => [],
+    'description' => 'It’s a little bit enchilada, a little bit chili and a whole lot of yummy!',
+    'image' => 'http://images-gmi-pmc.edge-generalmills.com/a7e5296a-dad5-4855-9b7d-44f58598b6b8.jpg',
+    'ingredients' => [
+        '1 package (20 oz) boneless skinless chicken thighs, cut into 1-inch pieces',
+        '1 can (15.2 oz) whole kernel sweet corn, drained, rinsed',
+        '1 can (15 oz) Progresso™ black beans, drained, rinsed',
+        '1 can (10 oz) Old El Paso™ mild enchilada sauce',
+        '2 tablespoons Old El Paso™ taco seasoning mix (from 1-oz package)',
+        '2 cups shredded Colby-Monterey Jack cheese blend (8 oz)',
+        'Chopped green onions and sour cream, if desired',
+        '4 cups tortilla chips',
+    ],
+    'instructions' => [
+        'Spray 4-quart slow cooker with cooking spray. In slow cooker, mix chicken, corn, beans, enchilada sauce and taco seasoning mix. Cover and cook on Low heat setting 8 hours or High heat setting 4 hours.',
+        'Stir in 1 cup of the cheese. Top with green onions and sour cream. Top with remaining cheese; serve with tortilla chips.',
+    ],
+    'name' => 'Slow-Cooker Cheesy Chicken Enchilada Chili',
+    'prepTime' => 'PT10M',
+    'publisher' => '',
+    'totalTime' => 'PT4H10M',
+    'url' => 'https://www.pillsbury.com/recipes/slow-cooker-cheesy-chicken-enchilada-chili/389d56ac-2840-4327-a23b-1303539a7248',
+    'yield' => '4',
+];
