196beedd6b52cf1e2e5173ac2a679be87e7845c2
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-01 11:35:23 -0700

    First pass as www.myrecipes.com results

---
+++ tests/data/results/www.myrecipes.com/recipe-quick-chicken-dumplings-0.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'author' => 'Melanie Barnard',
+    'categories' => [],
+    'cookingMethod' => '',
+    'cookTime' => '',
+    'cuisines' => [],
+    'description' => 'Flour tortillas stand in for the traditional biscuit dough so you can bring this crowd-pleaser from stove to table in minutes.',
+    'image' => 'http://cdn-image.myrecipes.com/sites/default/files/styles/300x300/public/quick-chicken-dumplings-mr.jpg?itok=7iuasoNn',
+    'ingredients' => [
+        '1 tablespoon butter',
+        '1/2 cup prechopped onion',
+        '2 cups chopped roasted skinless, boneless chicken breasts',
+        '1 (10-ounce) box frozen mixed vegetables, thawed',
+        '1 1/2 cups water',
+        '1 tablespoon all-purpose flour',
+        '1 (14-ounce) can fat-free, less-sodium chicken broth',
+        '1/4 teaspoon salt',
+        '1/4 teaspoon black pepper',
+        '1 bay leaf',
+        '8 (6-inch) flour tortillas, cut into 1/2-inch strips',
+        '1 tablespoon chopped fresh parsley',
+    ],
+    'instructions' => [
+        'Melt butter in a large saucepan over medium-high heat. Add onion; sauté 5 minutes or until tender. Stir in chicken and vegetables; cook 3 minutes or until thoroughly heated, stirring constantly.',
+        'While chicken mixture cooks, combine water, flour, and broth. Gradually stir broth mixture into chicken mixture. Stir in salt, pepper, and bay leaf; bring to a boil. Reduce heat, and simmer 3 minutes. Stir in tortilla strips, and cook 2 minutes or until tortilla strips soften. Remove from heat; stir in parsley. Discard bay leaf. Serve immediately.',
+    ],
+    'name' => 'Quick Chicken and Dumplings',
+    'prepTime' => '',
+    'publisher' => 'Cooking Light',
+    'totalTime' => '',
+    'url' => 'http://www.myrecipes.com/recipe/quick-chicken-dumplings-0',
+    'yield' => '4',
+];
