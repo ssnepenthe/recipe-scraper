dccb6b0063f223f2baf4a130ae5b67a0eda6a96c
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-01 11:59:57 -0700

    First pass at www.tablespoon.com results

---
+++ tests/data/results/www.tablespoon.com/recipes-bacon-avocado-lettuce-tomato-snack-sandwich-780d10c2-77e0-441b-9f75-1bb23e2081e0.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'author' => 'Daring Gourmet',
+    'categories' => [],
+    'cookingMethod' => '',
+    'cookTime' => '',
+    'cuisines' => [],
+    'description' => 'A BALT (bacon, avocado, lettuce, tomato) sandwich with a balsamic reduction drizzle is great for snack time or meal time.',
+    'image' => 'http://images-gmi-pmc.edge-generalmills.com/b1ab8df2-c235-4cd9-a577-723a5d3c2f3a.jpg',
+    'ingredients' => [
+        '1/3 cup good quality aged balsamic vinegar',
+        '4 slices bread, toasted or grilled',
+        'Mayonnaise',
+        '4 leaves lettuce',
+        '8 thick cut slices bacon, cooked',
+        '8 slices tomato',
+        '8 slices avocado',
+    ],
+    'instructions' => [
+        'To make the balsamic reduction, place the balsamic vinegar in a very small saucepan and bring it to a boil. Reduce the heat to medium-low and simmer uncovered for about 15 minutes or until it is thickened and lightly coats a spoon.',
+        'To assemble the sandwiches, spread some mayonnaise on each slice of toasted bread. Next layer with lettuce, 2 slices of bacon, 2 slices of tomato and 2 slices of avocado. Drizzle with the balsamic vinegar reduction and serve immediately.',
+    ],
+    'name' => 'Bacon-Avocado-Lettuce-Tomato Snack Sandwich',
+    'prepTime' => '',
+    'publisher' => '',
+    'totalTime' => 'PT15M',
+    'url' => 'http://www.tablespoon.com/recipes/bacon-avocado-lettuce-tomato-snack-sandwich/780d10c2-77e0-441b-9f75-1bb23e2081e0',
+    'yield' => '4',
+];
