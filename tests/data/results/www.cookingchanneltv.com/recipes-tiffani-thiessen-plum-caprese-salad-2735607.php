2a03442e79595562c2bd39b7b96c9cbc96fc3f51
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-01 08:00:31 -0700

    First pass at www.cookingchanneltv.com results

---
+++ tests/data/results/www.cookingchanneltv.com/recipes-tiffani-thiessen-plum-caprese-salad-2735607.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'author' => 'Tiffani Thiessen',
+    'categories' => [],
+    'cookingMethod' => '',
+    'cookTime' => '',
+    'cuisines' => [],
+    'description' => '',
+    'image' => 'http://cook.sndimg.com/content/dam/images/cook/fullset/2015/3/6/0/CCTIF110_Plum-Caprese-Salad-recipe_s4x3.jpg/jcr:content/renditions/cq5dam.web.266.200.jpeg',
+    'ingredients' => [
+        '1 tablespoon champagne vinegar',
+        '1 teaspoon white balsamic vinegar',
+        'Kosher salt and freshly ground black pepper',
+        '3 tablespoons olive oil',
+        '1 1/2 pounds heirloom tomatoes, various colors, cut into wedges',
+        '3 ripe plums, cut into wedges',
+        '8 ounces burrata cheese',
+        '1/4 cup fresh basil leaves, thinly sliced',
+        '10 fresh mint leaves, thinly sliced',
+        'Sea salt flakes, for garnishing',
+    ],
+    'instructions' => [
+        'Whisk together the champagne vinegar, white balsamic vinegar and a pinch of kosher salt. Stream the olive oil into the vinegar mixture, whisking; set aside. Arrange the tomatoes and plums on a serving platter, alternating the tomatoes and plums. Tear the buratta over the top. Sprinkle the basil and mint over the salad and then drizzle with the vinaigrette. Finish with a pinch of sea salt and some pepper; serve.'
+    ],
+    'name' => 'Plum Caprese Salad',
+    'prepTime' => 'PT10M',
+    'publisher' => '',
+    'totalTime' => 'PT10M',
+    'url' => 'http://www.cookingchanneltv.com/recipes/tiffani-thiessen/plum-caprese-salad.html',
+    'yield' => '6',
+];
