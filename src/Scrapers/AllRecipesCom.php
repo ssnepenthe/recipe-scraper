d055b9f78c80060137be0e7ebb82b4b438ff7b6c
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-02 11:49:50 -0700

    Update allrecipes.com scraper to new schema.org base

--- src/Scrapers/AllRecipesCom.php
+++ src/Scrapers/AllRecipesCom.php
@@ -2,16 +2,39 @@
 
 namespace SSNepenthe\RecipeScraper\Scrapers;
 
+use Symfony\Component\DomCrawler\Crawler;
+
 /**
  * @todo  Has notes if we want them.
  */
-class AllRecipesCom extends SchemaOrg
+class AllRecipesCom extends SchemaOrgMarkup
 {
-    protected function applyScraperConfig()
+    public function supports(Crawler $crawler) : bool
     {
-        parent::applyScraperConfig();
+        return 'allrecipes.com' === parse_url($crawler->getUri(), PHP_URL_HOST);
+    }
 
-        $this->config['recipeIngredients']['selector'] = '[itemprop="ingredients"]';
-        $this->config['recipeInstructions']['selector'] = '[itemprop="recipeInstructions"] li';
+    protected function extractDescription(Crawler $crawler)
+    {
+        return $this->makeExtractor(self::SINGULAR_EXTRACTOR)
+            ->extract($crawler, '[itemprop="description"]');
+    }
+
+    protected function extractIngredients(Crawler $crawler)
+    {
+        return $this->makeExtractor(self::PLURAL_EXTRACTOR)
+            ->extract($crawler, '[itemprop="ingredients"]');
+    }
+
+    protected function extractInstructions(Crawler $crawler)
+    {
+        return $this->makeExtractor(self::PLURAL_EXTRACTOR)
+            ->extract($crawler, '[itemprop="recipeInstructions"] li');
+    }
+
+    protected function extractName(Crawler $crawler)
+    {
+        return $this->makeExtractor(self::SINGULAR_EXTRACTOR)
+            ->extract($crawler, 'h1[itemprop="name"]');
     }
 }
