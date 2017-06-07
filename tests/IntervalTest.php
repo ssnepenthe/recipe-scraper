0a6971ff7e2cfa622edd5640d61d7483bd71b7ad
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-07 14:27:38 -0700

    Add tests for interval helpers

---
+++ tests/IntervalTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace RecipeScraperTests;
+
+use PHPUnit\Framework\TestCase;
+use SSNepenthe\RecipeScraper\Interval;
+
+class IntervalTest extends TestCase
+{
+	/** @test */
+	function it_can_create_intervals_using_standard_interval_spec()
+	{
+		$this->assertEquals(
+			new \DateInterval('PT30M'),
+			Interval::fromString('PT30M')
+		);
+	}
+
+	/** @test */
+	function it_can_create_intervals_from_relative_interval_strings()
+	{
+		$this->assertEquals(
+			new \DateInterval('PT30M'),
+			Interval::fromString('30 minutes')
+		);
+	}
+
+	/** @test */
+	function it_throws_for_bad_interval_strings()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+
+		Interval::fromString('not an interval');
+	}
+
+	/** @test */
+	function it_automatically_recalculates_carry_over()
+	{
+		$this->assertEquals(
+			new \DateInterval('PT1H15M'),
+			Interval::fromString('PT75M')
+		);
+	}
+
+	/** @test */
+	function it_can_format_interval_as_iso_8601()
+	{
+		$this->assertEquals(
+			'PT30M',
+			Interval::toIso8601(new \DateInterval('PT30M'))
+		);
+		$this->assertEquals(
+			'PT1H20M',
+			Interval::toIso8601(new \DateInterval('PT1H20M'))
+		);
+		$this->assertEquals(
+			'P1DT14H25M55S',
+			Interval::toIso8601(new \DateInterval('P1DT14H25M55S'))
+		);
+	}
+
+	/** @test */
+	function it_knows_when_an_interval_is_empty()
+	{
+		$this->assertTrue(
+			Interval::isEmpty(\DateInterval::createFromDateString('bad string'))
+		);
+		$this->assertFalse(Interval::isEmpty(
+			\DateInterval::createFromDateString('55 minutes')
+		));
+	}
+
+	/** @test */
+	function it_can_recalculate_carry_over()
+	{
+		$this->assertEquals(
+			new \DateInterval('PT2H25M'),
+			Interval::recalculateCarryOver(new \DateInterval('PT145M'))
+		);
+	}
+}
