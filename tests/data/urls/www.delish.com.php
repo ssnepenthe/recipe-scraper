1c1e333912d3486c00a9e65b48c1149eeb2e289d
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-06-16 17:47:17 -0700

    Add tests for delish.com in prep for move to Hearst Digital scraper

--- tests/data/urls/www.delish.com.php
+++ tests/data/urls/www.delish.com.php
@@ -5,4 +5,5 @@ return [
     'http://www.delish.com/cooking/recipe-ideas/recipes/a48545/chimichurri-steak-sandwich-recipe/',
     'http://www.delish.com/cooking/recipe-ideas/recipes/a44836/slutty-brownie-pie-recipe-cool-whip-desserts/',
     'http://www.delish.com/cooking/recipe-ideas/recipes/a44395/pumpkin-cheesecake-brownie-bars-recipe/',
+    'http://www.delish.com/cooking/recipe-ideas/recipes/a48411/balsamic-grilled-chicken-and-zucchini-recipe/',
 ];
