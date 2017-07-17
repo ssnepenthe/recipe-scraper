74e463928ae2f9d5e26de5bd0add917ac9f97051
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-07-17 13:23:22 -0700

    Forgot to add url to list for emerils.com tests

--- tests/data/urls/emerils.com.php
+++ tests/data/urls/emerils.com.php
@@ -7,4 +7,5 @@ return array (
   3 => 'http://emerils.com/129308/triple-coconut-cake-seven-minute-coconut-frosting',
   4 => 'http://emerils.com/128326/julia-julep',
   5 => 'http://emerils.com/120385/artichoke-and-spinach-dip',
+  6 => 'http://emerils.com/128492/caf%C3%A9-au-lait-cr%C3%A8me-br%C3%BBl%C3%A9e-cane-syrup-cake',
 );
