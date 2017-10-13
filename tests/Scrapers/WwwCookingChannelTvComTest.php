3e2c216d4c6d8fd449fbafd09acd741969b91623
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-10-13 13:22:11 -0700

    Create scripps group for phpunit

--- tests/Scrapers/WwwCookingChannelTvComTest.php
+++ tests/Scrapers/WwwCookingChannelTvComTest.php
@@ -5,6 +5,9 @@ namespace RecipeScraperTests\Scrapers;
 use RecipeScraperTests\ScraperTestCase;
 use RecipeScraper\Scrapers\ScrippsNetworks;
 
+/**
+ * @group scripps
+ */
 class WwwCookingChannelTvComTest extends ScraperTestCase
 {
     protected function getHost()
--- tests/Scrapers/WwwFoodNetworkComTest.php
+++ tests/Scrapers/WwwFoodNetworkComTest.php
@@ -5,6 +5,9 @@ namespace RecipeScraperTests\Scrapers;
 use RecipeScraperTests\ScraperTestCase;
 use RecipeScraper\Scrapers\ScrippsNetworks;
 
+/**
+ * @group scripps
+ */
 class WwwFoodNetworkComTest extends ScraperTestCase
 {
     protected function getHost()
