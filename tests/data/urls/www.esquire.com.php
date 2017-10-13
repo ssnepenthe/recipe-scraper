d8e99440b2dee90fef21e94395af31fb1c54a762
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-10-13 12:13:10 -0700

    Drop 4th of july dessert from esquire tests
    This particular recipes no longer contains any useful text - just an excerpt and picture.

--- tests/data/urls/www.esquire.com.php
+++ tests/data/urls/www.esquire.com.php
@@ -1,10 +1,9 @@
 <?php
 
 return array (
-  0 => 'http://www.esquire.com/food-drink/food/recipes/a54919/4th-of-july-dessert/',
-  1 => 'http://www.esquire.com/food-drink/food/recipes/a54674/chicken-pho-recipe/',
-  2 => 'http://www.esquire.com/food-drink/food/recipes/a5774/make-pancakes-0509/',
-  3 => 'http://www.esquire.com/food-drink/food/recipes/a55422/huevos-rancheros-sandwich-mexican-coffee-recipe/',
-  4 => 'http://www.esquire.com/food-drink/food/recipes/a45393/best-bloody-mary-recipe/',
-  5 => 'http://www.esquire.com/food-drink/food/recipes/a55635/coffee-ice-cream-sandwiches-recipe/',
+  0 => 'http://www.esquire.com/food-drink/food/recipes/a54674/chicken-pho-recipe/',
+  1 => 'http://www.esquire.com/food-drink/food/recipes/a5774/make-pancakes-0509/',
+  2 => 'http://www.esquire.com/food-drink/food/recipes/a55422/huevos-rancheros-sandwich-mexican-coffee-recipe/',
+  3 => 'http://www.esquire.com/food-drink/food/recipes/a45393/best-bloody-mary-recipe/',
+  4 => 'http://www.esquire.com/food-drink/food/recipes/a55635/coffee-ice-cream-sandwiches-recipe/',
 );
