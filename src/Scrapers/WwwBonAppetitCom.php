f59085f078a9cbab4caa2ea2548b905aa3667468
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-10-21 12:30:45 -0700

    Bonappetit: updated author, instruction selectors

--- src/Scrapers/WwwBonAppetitCom.php
+++ src/Scrapers/WwwBonAppetitCom.php
@@ -9,6 +9,9 @@ use RecipeScraper\ExtractsDataFromCrawler;
  * We lose out on ingredient group titles by using LD+JSON.
  *
  * Has notes but they are stuffed in with instructions.
+ *
+ * Template *seems* to be set up to allow for multiple contributors/authors per recipe - should keep
+ * an eye out for any recipes where this is the case.
  */
 class WwwBonAppetitCom extends SchemaOrgJsonLd
 {
@@ -32,7 +35,7 @@ class WwwBonAppetitCom extends SchemaOrgJsonLd
     protected function extractAuthor(Crawler $crawler, array $json)
     {
         // Seems to include author, company, city and state. Also used for photo credit...
-        return $this->extractString($crawler, '.recipe__credits + span');
+        return $this->extractString($crawler, '.contributor-name');
     }
 
     /**
@@ -53,9 +56,8 @@ class WwwBonAppetitCom extends SchemaOrgJsonLd
      */
     protected function extractInstructions(Crawler $crawler, array $json)
     {
-        // @todo 17 July 2017: it looks like they made some updates but forgot about ld+json...
-        // This should be revisited over the next few weeks to see if they have restored it.
-        return $this->extractArray($crawler, '.preparation__hed, .preparation__step');
+        // @todo 21 October 2017: LD+JSON is still broken - only appears to include group headings.
+        return $this->extractArray($crawler, '.steps-wrapper .subhed, .steps-wrapper .step');
     }
 
     /**
