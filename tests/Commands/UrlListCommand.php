d41d531b70241f88f603a92373af425362a8fae9
ssnepenthe <ssnepenthe@users.noreply.github.com>
2018-01-03 21:00:08 -0700

    Allow url:list to show all tested URLs

--- tests/Commands/UrlListCommand.php
+++ tests/Commands/UrlListCommand.php
@@ -17,31 +17,37 @@ class UrlListCommand extends Command
     {
         $this->setName('url:list')
             ->setDescription('List all test URLs for a given host')
-            ->addArgument('host', InputArgument::REQUIRED, 'The host to list test URLs for');
+            ->addArgument('host', InputArgument::OPTIONAL, 'The host to list test URLs for');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $host = $input->getArgument('host');
-        $urlListFile = $this->getUrlsDataFilePath($host);
 
-        if (! file_exists($urlListFile)) {
-            throw new \InvalidArgumentException(sprintf(
-                'URL list file [%s] for provided host [%s] does not exist',
-                $urlListFile,
-                $host
-            ));
+        if ($host) {
+            $urlListFile = $this->getUrlsDataFilePath($host);
+
+            if (! file_exists($urlListFile)) {
+                throw new \InvalidArgumentException(sprintf(
+                    'URL list file [%s] for provided host [%s] does not exist',
+                    $urlListFile,
+                    $host
+                ));
+            }
+
+            $urlList = (array) static::includeFile($urlListFile);
+        } else {
+            $urlList = $this->getTestUrls();
         }
 
         $io = new SymfonyStyle($input, $output);
-        $urlList = (array) static::includeFile($urlListFile);
 
-        if ($output->isDebug()) {
+        if ($output->isDebug() && $host) {
             $io->block('Host: ' . $host, 'DEBUG');
             $io->block('URL list file: ' . $urlListFile, 'DEBUG');
         }
 
-        $io->note(sprintf('Testing %s URLs for %s', count($urlList), $host));
+        $io->note(sprintf('%s URLs Total', count($urlList)));
         $io->listing($urlList);
     }
 }
