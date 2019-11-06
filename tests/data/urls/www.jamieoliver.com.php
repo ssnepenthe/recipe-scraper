3e9057297af6cd6bfcb093b3571bd8ab03025e5b
Mark Howe <mark@vedol.com>
2019-11-06 14:48:19 +0000

    [www.JamieOliver.com] Removing a recipe that's no-longer online

--- tests/data/urls/www.jamieoliver.com.php
+++ tests/data/urls/www.jamieoliver.com.php
@@ -2,11 +2,10 @@
 
 return array (
   0 => 'https://www.jamieoliver.com/recipes/game-recipes/bacon-wrapped-roast-partridge-pork-stuffing-lentils/',
-  1 => 'https://www.jamieoliver.com/recipes/pasta-recipes/tagliatelle-with-spinach-mascarpone-and-parmesan/',
-  2 => 'https://www.jamieoliver.com/recipes/vegetables-recipes/lentil-tabbouleh/',
-  3 => 'https://www.jamieoliver.com/recipes/vegetable-recipes/aubergine-dip/',
-  4 => 'https://www.jamieoliver.com/recipes/pasta-recipes/sausage-pasta-bake/',
-  5 => 'https://www.jamieoliver.com/recipes/vegetables-recipes/grilled-cauliflower-with-winter-pesto/',
-  6 => 'https://www.jamieoliver.com/recipes/pasta-recipes/cracker-ravioli/',
-  7 => 'https://www.jamieoliver.com/recipes/egg-recipes/scrambled-egg-omelette/',
+  1 => 'https://www.jamieoliver.com/recipes/vegetables-recipes/lentil-tabbouleh/',
+  2 => 'https://www.jamieoliver.com/recipes/vegetable-recipes/aubergine-dip/',
+  3 => 'https://www.jamieoliver.com/recipes/pasta-recipes/sausage-pasta-bake/',
+  4 => 'https://www.jamieoliver.com/recipes/vegetables-recipes/grilled-cauliflower-with-winter-pesto/',
+  5 => 'https://www.jamieoliver.com/recipes/pasta-recipes/cracker-ravioli/',
+  6 => 'https://www.jamieoliver.com/recipes/egg-recipes/scrambled-egg-omelette/',
 );
