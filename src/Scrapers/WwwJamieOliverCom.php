1aceac4cf3d7b12370a378e6e58719639188637f
snuzi <sabrinuzi@gmail.com>
2020-05-06 20:24:58 +0200

    Return zero based index array for cuisines

--- src/Scrapers/WwwJamieOliverCom.php
+++ src/Scrapers/WwwJamieOliverCom.php
@@ -65,7 +65,7 @@ class WwwJamieOliverCom extends SchemaOrgJsonLd
             return false === strpos($cuisine, 'schema.org');
         }));
 
-        return empty($cuisines) ? null : $cuisines;
+        return empty($cuisines) ? null : array_values($cuisines);
     }
 
     protected function extractIngredients(Crawler $crawler, array $json)
