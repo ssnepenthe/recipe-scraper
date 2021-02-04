3f58f3e67c1d383435ad7692c246dbaf6934a081
ssnepenthe <ssnepenthe@users.noreply.github.com>
2021-02-04 16:57:31 -0700

    Add some additional test cases for www.feastingathome.com

---
+++ tests/data/results/www.feastingathome.com/rosemary-olive-sourdough-bread.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    'author' => 'Sylvia Fountaine',
+    'categories' => [
+        'bread',
+    ],
+    'cookingMethod' => 'baked',
+    'cookTime' => 'PT40M',
+    'cuisines' => [
+        'northwest',
+    ],
+    'description' => 'How to make Crusty Sourdough Bread, infused with rosemary, roasted garlic and olives that requires no kneading and rises overnight. Made with sourdough starter instead of yeast! Allow 10-14 hours rising time!',
+    'image' => 'https://www.feastingathome.com/wp-content/uploads/2020/10/rosemary-olive-garlic-sourdough-bread-25-225x225.jpg',
+    'ingredients' => [
+        '4 cups organic white bread flour (520 grams) see notes',
+        '2 teaspoons sea salt (12 grams)',
+        '1/3 cup sourdough starter (use after it has peaked) 90 grams',
+        '1 7/8 cup water (385 grams)',
+        '3/4 cup chopped kalamata olives',
+        '2-3 tablespoons fresh rosemary, chopped',
+        '1/2 cup roasted whole garlic cloves (optional but good)',
+        'rice flour for dusting (or sub regular flour)',
+    ],
+    'instructions' => [
+        'Read through and perfect the No-Knead Sourdough Recipe (watch the video there) I recommend not attempting this until you have mastered that recipe. 🙂',
+        'Mix the dough At night, in a large bowl, mix flour and salt together. In a small bowl, mix starter and water together using a fork. Pour the starter-water into the flour mixture and stir with a fork until relatively combined. Using a wooden spoon, or dough mixer, scrape sides and mix dough for one minute, incorporating all the flour. The dough will feel thick and heavy and shaggy. Just do your best to incorporate all the flour. If for some reason the dough will not incorporate all the flour, add a tablespoon of water at a time to get it incorporated. Cover with a damp kitchen cloth, wait 15 minutes and do 2 sets of stretch and folds, 15 minutes apart. See video.',
+        'RISE Let rise overnight on the counter 10-14 hours. 65F-70F is ideal. In the morning, the dough should have swelled, but not quite doubled. Do the poke test.',
+        'Prepare your banneton dusting the banneton with rice flour and perhaps some olives and rosemary in the bottom ( which will end up being the top.) See notes for using a regular bowl.',
+        'SHAPE Using a wet dough scraper, or wet fingers, loosen the dough around the edges and pour it onto a well-floured surface. Treat the dough gently, flattening lightly with your fingertips into a rough rectangle about 1 1/2 inches thick. It need not be perfect. Sprinkle the olives, rosemary and garlic over the surface. Using the dough scraper, lift up one side, stretching just a bit up and over to the middle, then fold up the other side, like a tri-fold envelope. Turn the dough horizontally. Let the dough rest for 1 minute. Then repeat. Gently flatten with fingertips (it needn\'t be as flat as the first time), then tri-fold. Pinch the seams closed and lift into the prepared banneton, seam side up.',
+        'REST & PREHEAT Place dough in the fridge uncovered for 1 hour, while you preheat the oven to 500F (or 475F) with your lidded dutch oven inside.',
+        'SCORE Pull the dough from the fridge and have your lame (or sharp knife or razor blade) ready to score. Place a piece of parchment over the banneton and flip the dough, setting down on the counter over the parchment. Score 3/4 -1 inch deep at a 45-degree angle- basically wherever you want the dough to puff up. One slash is fine, or create a design.',
+        'Pull the dutch oven from the oven, and close the oven door. Remove the lid, place the scored dough into the dutch oven lifting it by the corners of the parchment. (See notes) Quickly cover and place back in the oven.',
+         'BAKE Place in the middle of the oven for 20 mins with convection on, 25 minutes w/no convection (or 28 minutes at 450F). Remove lid and it should be puffed and just lightly golden. Lower heat to 450 F, continue baking 10-15 minutes until deeply golden and internal temp reaches 202- 208F. No pale loaves please, let them get golden!',
+        'COOL Remove from the dutch oven, let it cool 1 hour on a rack or tilted up on its side, before slicing so you don\'t let the steam out and don\'t smash it- be patient. This is the hardest part. 😉. Take a picture! Feel proud. You did it!',
+    ],
+    'name' => 'Rosemary Olive Sourdough Bread',
+    'notes' => null, // @todo
+    'prepTime' => 'PT25M',
+    'publisher' => '',
+    'totalTime' => 'PT1H5M',
+    'url' => 'https://www.feastingathome.com/rosemary-olive-sourdough-bread/',
+    'yield' => null, // @todo
+];
---
+++ tests/data/results/www.feastingathome.com/simple-chicken-enchiladas.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'author' => 'Sylvia Fountaine',
+    'categories' => [
+        'Main',
+    ],
+    'cookingMethod' => 'baked',
+    'cookTime' => 'PT20M',
+    'cuisines' => [
+        'Mexican',
+    ],
+    'description' => 'How to make Simple Chicken Enchiladas with the BEST enchilada sauce! Fast and Easy with the 5-minute enchilada sauce!',
+    'image' => 'https://www.feastingathome.com/wp-content/uploads/2020/09/enchildadas-verde-7-225x225.jpg',
+    'ingredients' => [
+        'Shredded Chicken (you\'ll need 4 cups)',
+        '4-ounce can green chilies',
+        '5-minute Enchilada Sauce or Roasted Enchilada Sauce (2 cups)',
+        '8-10 tortillas (corn and flour blend)',
+        '2 cups cheese- cotija, shredded Mexican blend, shredded jack, etc.',
+        'optional filling additions: frozen corn, black beans, sauteed bell pepper, etc.',
+        'garnishes: olives, sour cream, cilantro, scallions, avocado',
+    ],
+    'instructions' => [
+        'Make the Shredded Chicken (or shred a rotisserie chicken-see notes) drain, set aside 4 cups of the chicken and stir in the small can of green chilies.',
+        // 'Preheat oven to 350F',
+        'Make the 5-Minute Enchilada Sauce',
+        'Heat the tortillas until they are pliable, to prevent cracking when rolling-especially if using corn tortillas. (In a microwave -wrap the stack in a kitchen towel for 20 seconds, or spread out on the rack in a 350 F oven, or lightly toast over a gas burner using tongs)',
+        'Grease a 9×13-inch baking dish. Spread 1/2 cup Enchilada sauce, or enough to coat the bottom.',
+        'Roll the Enchiladas Place 1/4 cup cheese down the center of a tortilla, top with 1/3-1/2 cup chicken filling all the way to the edges. Roll tightly and place seam side down on top of the enchilada sauce. Repeat until the baking dish is full.',
+        'Brush the remaining sauce over top.',
+        'Sprinkle with cheese.',
+        'Bake uncovered for 20-25 minutes or until warmed through and cheese is melty.',
+        'Top with garnishes before serving!',
+    ],
+    'name' => 'Simple Chicken Enchiladas',
+    'notes' => null, // @todo
+    'prepTime' => 'PT40M',
+    'publisher' => null,
+    'totalTime' => 'PT1H',
+    'url' => 'https://www.feastingathome.com/simple-chicken-enchiladas/',
+    'yield' => null, // @todo
+];
